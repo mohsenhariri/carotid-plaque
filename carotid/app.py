6765d8a1c8a139258726a37057a4d0f96143aa08
Mohsen Hariri <mhariri68@gmail.com>
2023-10-29 15:50:03 -0400

    check mask

--- carotid/app.py
+++ carotid/app.py
@@ -1,10 +1,8 @@
 import medviz as viz
 
-
 print(viz.__version__)
 
 mask = "dataset/air rt sinus+ cortical bone.nii.gz"
+image = "/storage/sync/git/mohsen/carotid-plaque/dataset/2-18-2021.dcm"
 
-mask = viz.im2arr(mask)
-
-print(mask.shape)
+viz.plot3d(images=image, masks=mask)
